45a859a14d99aa4011433103d8ee3ddbc250e330
Minsang Choi <radiofun8@gmail.com>
2025-08-07 17:54:21 -0700

    Initial Commit

---
+++ LightandTilt/LightandTiltApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightandTiltApp.swift
+//  LightandTilt
+//
+//  Created by Minsang Choi on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LightandTiltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
